9d6b3d0c8b4801c45b260bab9e6157a3067a1ca0
YoutingWang <youtingwang0825@gmail.com>
2020-12-03 20:35:18 -0800

    Updates

---
+++ precache-manifest.9ee85650f55ed64caad3d43b4d26e286.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40f3e70b431000819279a92ab5e9e897",
+    "url": "/seainfo6150-final-project-webapp/index.html"
+  },
+  {
+    "revision": "eb8002ad3763438cf89c",
+    "url": "/seainfo6150-final-project-webapp/static/css/main.094448b9.chunk.css"
+  },
+  {
+    "revision": "c3ebb03ad388efe61162",
+    "url": "/seainfo6150-final-project-webapp/static/js/2.5b6f1b1e.chunk.js"
+  },
+  {
+    "revision": "eb8002ad3763438cf89c",
+    "url": "/seainfo6150-final-project-webapp/static/js/main.caa95e59.chunk.js"
+  },
+  {
+    "revision": "940ec4a42937c37ae625",
+    "url": "/seainfo6150-final-project-webapp/static/js/runtime~main.c4d7f5e8.js"
+  },
+  {
+    "revision": "d74db5bfaaa593afef9a19071205c25b",
+    "url": "/seainfo6150-final-project-webapp/static/media/AboutMe.d74db5bf.JPG"
+  },
+  {
+    "revision": "18b909a7d0d1730a20ab1dfefc554063",
+    "url": "/seainfo6150-final-project-webapp/static/media/Logo.18b909a7.jpg"
+  },
+  {
+    "revision": "42a5836054bf40145e7d4483c821ff17",
+    "url": "/seainfo6150-final-project-webapp/static/media/about.42a58360.jpeg"
+  }
+]);
